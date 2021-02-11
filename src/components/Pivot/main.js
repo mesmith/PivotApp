16120eb79aa92535f9f040c64c017d03f42b9920
Mark Smith <mark_ekagra@bitbucket.org>
2021-02-11 11:10:36 -0500

    Remove spurious imports

--- src/components/Pivot/main.js
+++ src/components/Pivot/main.js
@@ -3,21 +3,9 @@ import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 
-import startup from './startup.js';
-
 import PivotApp from './app.js';
-
-import controls from './controls.js';
-import datapoint from './datapoint.js';
-import dataread from './dataread.js';
 import metadata from './metadata.js';
-
-import reducers from '../../reducers';
 import store from '../../store';
-import actions from '../../actions';
-
-import constants from './constants.js';
-import config from './config.js';
 
 // Turns off eslint warnings:
 /* global d3 */
@@ -30,6 +18,7 @@ if (module.hot) {
 // display the Loading icon.
 //
 window.onload = function(){
+console.error('window.onload: initial render');
 
   // This will display the Loading icon
   //
