9c8269c541bbc84764ecf70414032a180caba90b
Mark Smith <mark_ekagra@bitbucket.org>
2021-02-13 18:35:42 -0500

    Remove spurious console.log

--- src/reducers/pivotReducer.js
+++ src/reducers/pivotReducer.js
@@ -29,7 +29,6 @@ const getPreviousDataset = function(state) {
 }
 
 export default function(state = initialState, action) {
-console.log('reducer: action=' + action.type);
   switch( action.type ){
     case actionTypes.pivot_PushState:
     {
